618fa54cc22a4065b77ed3c765bd9b084ab5dc4c
Michael Stowe <michaelstowe12@gmail.com>
2024-08-22 00:02:11 -0400

    Testing middleware

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
+import {
+  getAuth,
+  oidcAuthMiddleware,
+  processOAuthCallback,
+  revokeSession,
+} from "@hono/oidc-auth";
 import { Hono } from "hono";
-import { options } from "./routes/options";
-import { hass } from "./routes/hass";
 import { cors } from "hono/cors";
+import { hass } from "./routes/hass";
+import { options } from "./routes/options";
+
+// ┌
+// │ Setup
+// └
 
 const app = new Hono();
 
@@ -12,9 +22,34 @@ app.use(
   })
 );
 
+// app.use("*", oidcAuthMiddleware());
+
 app.route("/options", options);
 app.route("/hass", hass);
 
+// ──────────────────────────────────────────────────────────────────────
+
+// ─[ For cron job to keep server alive ]──────────────────────────────
 app.get("/wake", (c) => c.text("Wakey wakey!"));
+
+// ┌
+// │ Auth
+// └
+
+app.get("/logout", async (c) => {
+  await revokeSession(c);
+  return c.text("Successfully logged out.");
+});
+
+app.get("/callback", async (c) => {
+  return processOAuthCallback(c);
+});
+
+app.get("/", async (c) => {
+  const auth = await getAuth(c);
+  return c.text(`Hello <${auth?.email}>!`);
+});
+
+// ──────────────────────────────────────────────────────────────────────
 
 export default app;
